e6252e19ddb258fdf9c767c91dc964a9d850546e
georgidelchev <alienguymc@gmail.com>
2020-08-20 20:47:52 +0300

    add

---
+++ 01 - [Stacks and Queues - Lab]/01 - [Reverse Strings]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Any())
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
