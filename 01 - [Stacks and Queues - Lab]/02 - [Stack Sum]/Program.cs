349a43c928eda6c3c38a4ea3bcf878dc66c14a21
georgidelchev <alienguymc@gmail.com>
2020-08-21 20:52:09 +0300

    add

---
+++ 01 - [Stacks and Queues - Lab]/02 - [Stack Sum]/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var stack = new Stack<int>(numbers);
+
+            string input;
+            while ((input = Console.ReadLine().ToLower()) != "end")
+            {
+                var splittedInput = input
+                    .ToLower()
+                    .Split()
+                    .ToList();
+
+                string command = splittedInput[0];
+
+                switch (command)
+                {
+                    case "add":
+
+                        int firstNumber = int.Parse(splittedInput[1]);
+                        int secondNumber = int.Parse(splittedInput[2]);
+
+                        stack.Push(firstNumber);
+                        stack.Push(secondNumber);
+                        break;
+                    case "remove":
+                        int indexes = int.Parse(splittedInput[1]);
+
+                        if (stack.Count >= indexes)
+                        {
+                            for (int i = 0; i < indexes; i++)
+                            {
+                                stack.Pop();
+                            }
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
