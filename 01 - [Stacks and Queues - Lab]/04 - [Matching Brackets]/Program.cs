032db684b8669264e3125dce56a3cc366d46dcd3
georgidelchev <alienguymc@gmail.com>
2020-08-22 14:46:24 +0300

    add

---
+++ 01 - [Stacks and Queues - Lab]/04 - [Matching Brackets]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int currIndex = i;
+                    int openingBracket = stack.Pop();
+
+                    string sub = expression.Substring(openingBracket, currIndex - openingBracket + 1);
+                    Console.WriteLine(sub);
+                }
+            }
+        }
+    }
+}
