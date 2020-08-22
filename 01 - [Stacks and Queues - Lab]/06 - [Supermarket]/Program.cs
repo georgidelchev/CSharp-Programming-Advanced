e1479f7a2068149b1580cd4687172c0fce79476d
georgidelchev <alienguymc@gmail.com>
2020-08-22 15:09:01 +0300

    add

---
+++ 01 - [Stacks and Queues - Lab]/06 - [Supermarket]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, queue));
+                    queue.Clear();
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
