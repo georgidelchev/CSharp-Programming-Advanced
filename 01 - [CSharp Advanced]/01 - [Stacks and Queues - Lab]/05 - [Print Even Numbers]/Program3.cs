296a8485903bfae8d518c13404278126dbb38413
georgidelchev <alienguymc@gmail.com>
2020-09-19 16:08:11 +0300

    add

---
+++ 01 - [CSharp Advanced]/01 - [Stacks and Queues - Lab]/05 - [Print Even Numbers]/Program3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var queue = new Queue<int>(numbers);
+            Console.WriteLine(string.Join(", ", queue.Where(x => x % 2 == 0)));
+        }
+    }
+}
