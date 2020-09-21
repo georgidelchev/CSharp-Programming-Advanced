9e6da7cbc39718bd4cf22b95a1331c8c07f609b3
georgidelchev <alienguymc@gmail.com>
2020-09-21 20:07:58 +0300

    add

---
+++ 01 - [CSharp Advanced]/02 - [Stacks and Queues - Exercise]/07 - [Truck Tour]/Program2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumpsCount = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<List<int>>();
+
+            for (int i = 0; i < petrolPumpsCount; i++)
+            {
+                var pumpInfo = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                pumpInfo.Add(i);
+
+                queue.Enqueue(pumpInfo);
+            }
+
+            while (true)
+            {
+                bool isCompleted = true;
+                int totalFuel = 0;
+
+                foreach (var item in queue)
+                {
+                    totalFuel += item[0];
+
+                    if (totalFuel >= item[1])
+                    {
+                        totalFuel -= item[1];
+                    }
+                    else
+                    {
+                        isCompleted = false;
+                        break;
+                    }
+                }
+
+                if (isCompleted)
+                {
+                    break;
+                }
+
+                queue.Enqueue(queue.Dequeue());
+            }
+
+            var currentPump = queue.Dequeue().ToList()[2];
+
+            Console.WriteLine(currentPump);
+        }
+    }
+}
