5a99373a77e361e64e7f55125771d540ced2e508
georgidelchev <alienguymc@gmail.com>
2020-09-26 20:06:29 +0300

    add

---
+++ 01 - [CSharp Advanced]/04 - [Multidimensional Arrays - Exercise]/01 - [Diagonal Difference]/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var matrix = new int[size, size];
+
+            FillingUpIntMatrix(matrix);
+
+            Console.WriteLine(Math.Abs(FindingDiagonalsSums(size, matrix)));
+        }
+
+        static void FillingUpIntMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var elements = Console
+                    .ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+        }
+
+        static int FindingDiagonalsSums(int size, int[,] matrix)
+        {
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                primaryDiagonalSum += matrix[row, row];
+                secondaryDiagonalSum += matrix[row, size - row - 1];
+            }
+
+            return primaryDiagonalSum - secondaryDiagonalSum;
+        }
+    }
+}
