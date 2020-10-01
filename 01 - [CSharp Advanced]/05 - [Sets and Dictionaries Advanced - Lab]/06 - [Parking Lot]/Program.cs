ceb84beb2a5dddeeabacc4df6fe17ff27d1fa043
georgidelchev <alienguymc@gmail.com>
2020-10-01 22:42:33 +0300

    add

---
+++ 01 - [CSharp Advanced]/05 - [Sets and Dictionaries Advanced - Lab]/06 - [Parking Lot]/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new HashSet<string>();
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var splittedInput = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string command = splittedInput[0];
+                string carPlate = splittedInput[1];
+
+                switch (command)
+                {
+                    case "IN":
+                        set.Add(carPlate);
+                        break;
+                    case "OUT":
+                        set.Remove(carPlate);
+                        break;
+                }
+            }
+
+            if (set.Count() == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, set));
+            }
+        }
+    }
+}
