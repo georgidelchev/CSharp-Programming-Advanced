0942f7e97228277a6f402dfb6a0e6653a0100f6e
georgidelchev <alienguymc@gmail.com>
2020-10-01 23:15:18 +0300

    add

---
+++ 01 - [CSharp Advanced]/05 - [Sets and Dictionaries Advanced - Lab]/07 - [SoftUni Party]/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regulars = new HashSet<string>();
+            var vips = new HashSet<string>();
+
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "PARTY")
+                {
+                    while ((input = Console.ReadLine()) != "END")
+                    {
+                        if (regulars.Contains(input))
+                        {
+                            regulars.Remove(input);
+                        }
+                        if (vips.Contains(input))
+                        {
+                            vips.Remove(input);
+                        }
+                    }
+
+                    break;
+                }
+                else
+                {
+                    if (input[0] >= 48 && input[0] <= 57)
+                    {
+                        vips.Add(input);
+                    }
+                    else
+                    {
+                        regulars.Add(input);
+                    }
+                }
+            }
+            if (input == "END")
+            {
+                Console.WriteLine(regulars.Count() + vips.Count());
+                if (vips.Count() > 0)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, vips));
+                }
+                if (regulars.Count() > 0)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, regulars));
+                }
+            }
+
+        }
+    }
+}
