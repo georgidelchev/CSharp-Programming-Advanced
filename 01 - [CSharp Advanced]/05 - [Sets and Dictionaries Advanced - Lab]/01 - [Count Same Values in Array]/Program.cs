febb37b75b3ca10930b84955e1043ed7801bc31b
georgidelchev <alienguymc@gmail.com>
2020-10-01 19:21:15 +0300

    add

---
+++ 01 - [CSharp Advanced]/05 - [Sets and Dictionaries Advanced - Lab]/01 - [Count Same Values in Array]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<double, int>();
+
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!dict.ContainsKey(numbers[i]))
+                {
+                    dict[numbers[i]] = 0;
+                }
+
+                dict[numbers[i]]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
