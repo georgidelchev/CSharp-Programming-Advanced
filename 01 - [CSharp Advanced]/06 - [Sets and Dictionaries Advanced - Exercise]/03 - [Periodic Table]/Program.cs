42773c9507a7504b3a4ff8d013a0f4631e1cfc89
georgidelchev <alienguymc@gmail.com>
2020-10-01 23:43:01 +0300

    add

---
+++ 01 - [CSharp Advanced]/06 - [Sets and Dictionaries Advanced - Exercise]/03 - [Periodic Table]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new SortedSet<string>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console
+                    .ReadLine()
+                    .Split()
+                    .ToList();
+
+                for (int j = 0; j < input.Count; j++)
+                {
+                    set.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",set));
+        }
+    }
+}
