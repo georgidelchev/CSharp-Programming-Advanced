dae1029d89b17ddcfc69b9b14ab7ea891ad4ca5d
georgidelchev <alienguymc@gmail.com>
2020-10-01 23:25:48 +0300

    add

---
+++ 01 - [CSharp Advanced]/06 - [Sets and Dictionaries Advanced - Exercise]/01 - [Unique Usernames]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                set.Add(input);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, set));
+        }
+    }
+}
