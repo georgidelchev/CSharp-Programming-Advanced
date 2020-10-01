d624db84e3d0e14e2efb5c83d047f8e6f2a10ec6
georgidelchev <alienguymc@gmail.com>
2020-10-01 23:54:53 +0300

    add

---
+++ 01 - [CSharp Advanced]/06 - [Sets and Dictionaries Advanced - Exercise]/05 - [Count Symbols]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            var dict = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!dict.ContainsKey(str[i]))
+                {
+                    dict[str[i]] = 0;
+                }
+
+                dict[str[i]]++;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,
+                dict.Select(x => $"{x.Key}: {x.Value} time/s")));
+        }
+    }
+}
