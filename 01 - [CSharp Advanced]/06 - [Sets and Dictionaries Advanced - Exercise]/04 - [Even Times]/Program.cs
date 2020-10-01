eff7b42118e1ca410a035efa39dea9484c0ff95c
georgidelchev <alienguymc@gmail.com>
2020-10-01 23:50:37 +0300

    add

---
+++ 01 - [CSharp Advanced]/06 - [Sets and Dictionaries Advanced - Exercise]/04 - [Even Times]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(num))
+                {
+                    dict[num] = 0;
+                }
+
+                dict[num]++;
+            }
+
+            dict = dict
+                .Where(x => x.Value % 2 == 0)
+                .ToDictionary(x => x.Key, x => x.Value);
+            
+            Console.WriteLine(string.Join(" ", dict.Keys));
+        }
+    }
+}
