4537c334c298bb183c6333aa68bd14d2d4705bc0
Georgi <alienguymc@gmail.com>
2020-10-05 09:55:56 +0300

    add 2/6 files

---
+++ 01 - [CSharp Advanced]/08 - [Streams, Files and Directories - Exercise]/SoftUniHomeworkStreamsFilesAndDirectories/01.EvenLines/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("EvenLines", "text.txt");
+            var dir = Path.Combine("EvenLines", "output.txt");
+
+            var charsToReplace = new char[] { '-', ',', '.', '!', '?' };
+
+            char charForReplacing = '@';
+            using (var reader = new StreamReader(path))
+            {
+                var line = "";
+                int counter = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var replacedString = charsToReplace
+                        .Aggregate(line, (x1, x2) => x1.Replace(x2, charForReplacing))
+                        .Split()
+                        .ToList();
+
+                    replacedString.Reverse();
+
+                    if (counter % 2 == 0)
+                    {
+                        using (var writer = new StreamWriter(dir, true))
+                        {
+                            Console.Write(string.Join(" ", replacedString));
+                            writer.Write(string.Join(" ", replacedString));
+
+                            Console.WriteLine();
+                            writer.WriteLine();
+                        }
+                    }
+
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/08 - [Streams, Files and Directories - Exercise]/SoftUniHomeworkStreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("LineNumbers", "text.txt");
+            var dir = Path.Combine("LineNumbers", "output.txt");
+
+            var file = File.ReadAllLines(path).ToList();
+            for (int i = 0; i < file.Count; i++)
+            {
+                var lettersPattern = @"[A-Za-z]";
+                var lettersCount = Regex.Matches(file[i], lettersPattern);
+
+                var punctsPattern = @"[\.\,\!\?\-\']";
+                var punctsCount = Regex.Matches(file[i], punctsPattern);
+
+                file[i] = $"Line {i + 1}:{file[i]} ({lettersCount.Count})({punctsCount.Count})";
+
+                Console.WriteLine(file[i]);
+            }
+
+            File.WriteAllLines(dir, file);
+        }
+    }
+}
