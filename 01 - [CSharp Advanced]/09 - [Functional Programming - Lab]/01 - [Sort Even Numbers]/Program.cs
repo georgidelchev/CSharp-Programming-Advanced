033e2e7dee784885ff0652e25fae37dd59d292fe
Georgi <alienguymc@gmail.com>
2020-10-06 19:13:57 +0300

    add

---
+++ 01 - [CSharp Advanced]/09 - [Functional Programming - Lab]/01 - [Sort Even Numbers]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterByAge​
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
