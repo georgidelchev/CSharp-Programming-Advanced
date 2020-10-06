a5e1125569bfc02144ec945ddfbc773d5289daf1
Georgi <alienguymc@gmail.com>
2020-10-06 21:21:37 +0300

    add

---
+++ 01 - [CSharp Advanced]/09 - [Functional Programming - Lab]/02 - [Sum Numbers]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> func = int.Parse;
+
+            var nums = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(func)
+                .ToList();
+
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
