eb0a7547bd2529569b0835444fcf8d37576eca88
Georgi <alienguymc@gmail.com>
2020-10-07 20:07:25 +0300

    add

---
+++ 01 - [CSharp Advanced]/09 - [Functional Programming - Lab]/03 - [Count Uppercase Words]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => char.IsUpper(x[0]))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
