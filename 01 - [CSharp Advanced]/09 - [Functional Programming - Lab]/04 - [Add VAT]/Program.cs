a193ea7ecfa1b031b61bb6c00f2088e48d23d8fa
Georgi <alienguymc@gmail.com>
2020-10-07 20:15:48 +0300

    add

---
+++ 01 - [CSharp Advanced]/09 - [Functional Programming - Lab]/04 - [Add VAT]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console
+                .ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
