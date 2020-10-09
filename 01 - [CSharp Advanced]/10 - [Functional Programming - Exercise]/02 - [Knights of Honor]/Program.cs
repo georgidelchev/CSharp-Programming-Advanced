115105c97317d6f3bd96180f49c1256a5a545214
georgidelchev <alienguymc@gmail.com>
2020-10-09 15:39:37 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/02 - [Knights of Honor]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console
+                .ReadLine()
+                .Split()
+                .Select(x => $"Sir {x}")
+                .ToList();
+
+            Action<List<string>> print = n => n.ForEach(Console.WriteLine);
+            print(names);
+        }
+    }
+}
