f225d19191fa199e85d05a1c01cea573c809d2c2
georgidelchev <alienguymc@gmail.com>
2020-10-09 16:37:37 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/05 - [Applied Arithmetics]/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Action<List<int>> printingNumbers = x => Console.WriteLine(string.Join(" ", x));
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                Func<int, int> func = input switch
+                {
+                    "add" => x => x + 1,
+                    "multiply" => x => x * 2,
+                    "subtract" => x => x - 1,
+                    _ => null
+                };
+
+                if (input == "print")
+                {
+                    printingNumbers(numbers);
+                    continue;
+                }
+
+                numbers = numbers.Select(func).ToList();
+            }
+        }
+    }
+}
