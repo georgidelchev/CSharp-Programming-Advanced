1df09616c5feeaad2f3449d8c934252782e30e15
georgidelchev <alienguymc@gmail.com>
2020-10-09 16:45:16 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/07 - [Predicate For Names]/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var names = Console
+                .ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, bool> func = x => x.Length <= n;
+
+            names.Where(func)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
