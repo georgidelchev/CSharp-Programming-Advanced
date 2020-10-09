27ab8a5741888da3db42b0a3449687570a545de6
georgidelchev <alienguymc@gmail.com>
2020-10-09 16:12:01 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/04 - [Find Evens or Odds]/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var criteria = Console.ReadLine();
+
+            int start = numbers[0];
+            int end = numbers[1];
+
+            Func<int, int, List<int>> addingNumbers = (x, y) =>
+              {
+                  var list = new List<int>();
+
+                  for (int i = x; i <= y; i++)
+                  {
+                      list.Add(i);
+                  }
+
+                  return list;
+              };
+
+            var currentNumbers = addingNumbers(start, end);
+
+            Func<int, bool> func = criteria switch
+            {
+                "odd" => x => x % 2 != 0,
+                "even" => x => x % 2 == 0,
+            };
+
+            Console.WriteLine(string.Join(" ", currentNumbers.Where(func)));
+        }
+    }
+}
