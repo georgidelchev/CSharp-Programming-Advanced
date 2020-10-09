ab675a9e4f9aa7a1c337b9017774ad7aea37841d
georgidelchev <alienguymc@gmail.com>
2020-10-09 15:48:36 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/03 - [Custom Min Function]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, int> minFunc = x =>
+             {
+                 int minValue = int.MaxValue;
+
+                 foreach (var num in x)
+                 {
+                     if (num < minValue)
+                     {
+                         minValue = num;
+                     }
+                 }
+
+                 return minValue;
+             };
+
+            Console.WriteLine(minFunc(names));
+        }
+    }
+}
