4dcf7cbcde79b58262584d0c01ccf0c918263b4b
georgidelchev <alienguymc@gmail.com>
2020-10-09 17:46:32 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/09 - [List Of Predicates]/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, List<int>, List<int>> addNumbersFunc = (x, y) =>
+             {
+                 var list = new List<int>();
+
+                 for (int i = 1; i <= end; i++)
+                 {
+                     bool isValid = true;
+
+                     for (int j = 0; j < y.Count; j++)
+                     {
+                         if (!(i % y[j] == 0))
+                         {
+                             isValid = false;
+                         }
+                     }
+
+                     if (isValid)
+                     {
+                         list.Add(i);
+                     }
+                 }
+
+                 return list;
+             };
+
+            var numsList = addNumbersFunc(end, nums);
+
+
+            Console.WriteLine(string.Join(" ", numsList));
+        }
+    }
+}
