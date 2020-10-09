1080318c9119064cee046b05cd9b75a0fe85f09f
georgidelchev <alienguymc@gmail.com>
2020-10-09 15:35:43 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/01 - [Action Print]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console
+                .ReadLine()
+                .Split()
+                .ToList();
+
+            Action<List<string>> print = n => n.ForEach(Console.WriteLine);
+            print(names);
+        }
+    }
+}
