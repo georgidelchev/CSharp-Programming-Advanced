fd89052934ecc8493e026c3473e4df734d1a2403
georgidelchev <alienguymc@gmail.com>
2020-10-09 16:56:22 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/08 - [Custom Comparator]/Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(nums, (x, y) =>
+            x % 2 == 0 && y % 2 != 0 ? -1 : 
+            x % 2 != 0 && y % 2 == 0 ? 1 : 
+            x.CompareTo(y));
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
