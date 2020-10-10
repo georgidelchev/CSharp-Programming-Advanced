bdd458541763c0cdde276dc8727185d309016d13
georgidelchev <alienguymc@gmail.com>
2020-10-10 19:29:11 +0300

    add

---
+++ 01 - [CSharp Advanced]/10 - [Functional Programming - Exercise]/12 - [TriFunction]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minSum = int.Parse(Console.ReadLine());
+
+            var names = Console
+                .ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, int, bool> nameSum = (x, y) => x.Select(c => (int)c).Sum() >= y;
+
+            Func<List<string>, Func<string, int, bool>, string> name = (x, y) =>
+            {
+                string result = " ";
+
+                for (int i = 0; i < x.Count; i++)
+                {
+                    if (y(x[i], minSum))
+                    {
+                        result = x[i];
+                        break;
+                    }
+                }
+
+                return result;
+            };
+
+            Console.WriteLine(name(names, nameSum));
+        }
+    }
+}
