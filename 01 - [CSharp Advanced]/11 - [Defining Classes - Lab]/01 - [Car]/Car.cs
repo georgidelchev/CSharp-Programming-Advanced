024c07fec1c87bdccbe6e4235795560de91dedbd
Georgi <alienguymc@gmail.com>
2020-10-11 21:02:25 +0300

    add

---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/01 - [Car]/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/01 - [Car]/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}{Environment.NewLine}" +
+                $"Model: {car.Model}{Environment.NewLine}" +
+                $"Year: {car.Year}");
+        }
+    }
+}
