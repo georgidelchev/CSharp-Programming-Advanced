bd1ac0c3163babc5a70de182f5780af74ad8f130
Georgi <alienguymc@gmail.com>
2020-10-11 22:25:01 +0300

    add

---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/03 - [Car Constructors]/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private int year;
+
+        public Car()
+            : this("VW", "Golf", 2025, 200, 10)
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make,
+            string model,
+            int year,
+            double fuelQuantity,
+            double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(int distance)
+        {
+            double neededFuel = distance * this.FuelConsumption;
+
+            if (this.FuelQuantity - neededFuel <= 0)
+            {
+                throw new InvalidOperationException("No fuel!");
+            }
+
+            this.FuelQuantity -= neededFuel;
+        }
+
+        public string TotalCarInformation()
+        {
+            return $"Make: {this.Make}{Environment.NewLine}" +
+            $"Model: {this.Model}{Environment.NewLine}" +
+            $"Year: {this.Year}{Environment.NewLine}" +
+            $"FuelQuantity: {this.FuelQuantity}";
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/03 - [Car Constructors]/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            var car = new Car();
+            var secondCar = new Car(make, model, year);
+            var thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+        }
+    }
+}
