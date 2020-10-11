f40709f9ca43199a1a80e3c3b9dfe2bc89394990
Georgi <alienguymc@gmail.com>
2020-10-11 22:46:34 +0300

    add

---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/04 - [Car Engine And Tires]/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4]
+            {
+                new Tire(2020,3),
+                new Tire(2020,3),
+                new Tire(2020,3),
+                new Tire(2020,3)
+            };
+
+            var engine = new Engine(800, 12.0);
+
+            var car = new Car("Audi", "A4", 2020, 250, 100, engine, tires);
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/04 - [Car Engine And Tires]/Tire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public int Year { get; set; }
+
+        public double Pressure { get; set; }
+
+        public Tire(int year,double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+}
