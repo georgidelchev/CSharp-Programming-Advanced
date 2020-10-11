6e41a2738579c43ad75c264966e378875dbb431a
Georgi <alienguymc@gmail.com>
2020-10-11 21:17:24 +0300

    add

---
+++ 01 - [CSharp Advanced]/11 - [Defining Classes - Lab]/02 - [Car Extension]/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = fuelConsumption * distance;
+
+            if (this.fuelQuantity - neededFuel <= 0)
+            {
+                throw new InvalidOperationException("Not enough fuel!");
+            }
+
+            this.fuelQuantity -= neededFuel;
+        }
+
+        public string TotalCarInformation()
+        {
+            return $"Make: {this.Make}{Environment.NewLine}" +
+                $"Model: {this.Model}{Environment.NewLine}" +
+                $"Year: {this.Year}{Environment.NewLine}" +
+                $"Fuel: {this.FuelQuantity:f2}";
+        }
+    }
+}
