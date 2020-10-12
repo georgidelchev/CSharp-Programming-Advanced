99fa9066b928263b62a84c07ad0244c1f83f5cf2
Georgi <alienguymc@gmail.com>
2020-10-12 20:38:51 +0300

    add

---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/02 - [Creating Constructors]/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+            : this("No name", 1)
+        {
+
+        }
+
+        public Person(int age)
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/02 - [Creating Constructors]/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            Console.WriteLine(person.Name + " " + person.Age);
+
+            var secondPerson = new Person(15);
+
+            Console.WriteLine(secondPerson.Name + " " + secondPerson.Age);
+
+            var thirdPerson = new Person("Dimitrichko", 4);
+
+            Console.WriteLine(thirdPerson.Name + " " + thirdPerson.Age);
+
+        }
+    }
+}
