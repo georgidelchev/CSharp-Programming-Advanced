1bf15d6d424d733253507b455b9f45253eb3a3ff
Georgi <alienguymc@gmail.com>
2020-10-12 22:02:34 +0300

    add

---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/05 - [Date Modifier]/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public int CalculateDaysDifferenceBetweenTwoDates(string firstDate, string secondDate)
+        {
+            var firstDateArgs = firstDate.Split().Select(int.Parse).ToList();
+            var secondDateArgs = secondDate.Split().Select(int.Parse).ToList();
+
+            DateTime firstDateTime = new DateTime(firstDateArgs[0], firstDateArgs[1], firstDateArgs[2]);
+            DateTime secondDateTim = new DateTime(secondDateArgs[0], secondDateArgs[1], secondDateArgs[2]);
+            
+            return Math.Abs((int)firstDateTime.Subtract(secondDateTim).TotalDays);
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/05 - [Date Modifier]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            Console.WriteLine(dateModifier.CalculateDaysDifferenceBetweenTwoDates(firstDate, secondDate));
+        }
+    }
+}
