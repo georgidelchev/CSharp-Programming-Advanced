f2fb31993fb77bce2e52ac300b319fc3e13b0855
Georgi <alienguymc@gmail.com>
2020-10-12 20:30:25 +0300

    add

---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/01 - [Define a Class Person]/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+            : this("Unknown", 0)
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/12 - [Defining Classes - Exercise]/01 - [Define a Class Person]/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            Console.WriteLine(person.Name + " " + person.Age);
+
+            var forthPerson = new Person();
+            forthPerson.Name = "Pesho";
+            forthPerson.Age = 20;
+            Console.WriteLine(forthPerson.Name + " " + forthPerson.Age);
+
+            var secondPerson = new Person()
+            {
+                Name = "Gosho",
+                Age = 18
+            };
+            Console.WriteLine(secondPerson.Name + " " + secondPerson.Age);
+
+            var thirdPerson = new Person("Stamat", 43);
+            Console.WriteLine(thirdPerson.Name + " " + thirdPerson.Age);
+
+        }
+    }
+}
