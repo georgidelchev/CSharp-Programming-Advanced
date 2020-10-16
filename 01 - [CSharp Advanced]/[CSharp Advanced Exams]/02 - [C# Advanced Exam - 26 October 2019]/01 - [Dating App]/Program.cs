75a6b99c5226e6d2f6e4ff63d3899f680a83e09c
george <alienguymc@gmail.com>
2020-10-17 00:12:57 +0300

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/02 - [C# Advanced Exam - 26 October 2019]/01 - [Dating App]/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var males = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 25 != 0)
+                .Where(x => x > 0)
+                .ToList();
+
+            var females = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 25 != 0)
+                .Where(x => x > 0)
+                .ToList();
+
+            var femalesQueue = new Queue<int>(females);
+            var malesStack = new Stack<int>(males);
+
+            int matchesCount = 0;
+
+            while (femalesQueue.Any() && malesStack.Any())
+            {
+                if (femalesQueue.Any() && malesStack.Any())
+                {
+                    if (femalesQueue.Peek() == malesStack.Peek())
+                    {
+                        femalesQueue.Dequeue();
+                        malesStack.Pop();
+
+                        matchesCount++;
+                    }
+                    else
+                    {
+                        femalesQueue.Dequeue();
+
+                        if (malesStack.Peek() - 2 <= 0)
+                        {
+                            malesStack.Pop();
+                        }
+                        else
+                        {
+                            malesStack.Push(malesStack.Pop() - 2);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Matches: {matchesCount}");
+
+            Console.Write("Males left: ");
+            Console.WriteLine(malesStack.Any() ? string.Join(", ", malesStack) : "none");
+
+            Console.Write("Females left: ");
+            Console.WriteLine(femalesQueue.Any() ? string.Join(", ", femalesQueue) : "none");
+        }
+    }
+}
