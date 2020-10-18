2848dbcc50f4536eefc626442cd75b9f89d402c9
george <alienguymc@gmail.com>
2020-10-18 13:55:05 +0300

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/05 - [C# Advanced Retake Exam - 19 August 2020]/03 - [VetClinic]/Clinic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> petsList;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.petsList = new List<Pet>();
+        }
+
+        public int Count
+        {
+            get => this.petsList.Count;
+        }
+
+        public int Capacity { get; set; }
+
+        public void Add(Pet pet)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.petsList.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = this.petsList.FirstOrDefault(x => x.Name == name);
+
+            if (pet != null)
+            {
+                this.petsList.Remove(pet);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = this.petsList.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet pet = this.petsList.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return pet;
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var item in this.petsList)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/05 - [C# Advanced Retake Exam - 19 August 2020]/03 - [VetClinic]/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}";
+        }
+    }
+}
