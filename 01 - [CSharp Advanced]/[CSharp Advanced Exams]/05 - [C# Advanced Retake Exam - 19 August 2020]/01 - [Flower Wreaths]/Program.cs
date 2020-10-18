e6efadc706070f5ff90665a15f75a39761f77b89
george <alienguymc@gmail.com>
2020-10-18 13:55:23 +0300

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/05 - [C# Advanced Retake Exam - 19 August 2020]/01 - [Flower Wreaths]/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstSequence = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            var secondSequence = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            var lilies = new Stack<int>(secondSequence);
+            var roses = new Queue<int>(firstSequence);
+
+            int wreathsCount = 0;
+            int storedFlowers = 0;
+
+            while (lilies.Any() && roses.Any())
+            {
+                int firstLilie = lilies.Peek();
+                int firstRose = roses.Peek();
+
+                int flowersSum = firstLilie + firstRose;
+
+                if (flowersSum == 15)
+                {
+                    lilies.Pop();
+                    roses.Dequeue();
+
+                    wreathsCount++;
+                }
+                else if (flowersSum >= 15)
+                {
+                    lilies.Push(lilies.Pop() - 2);
+                }
+                else
+                {
+                    storedFlowers += lilies.Pop() + roses.Dequeue();
+                }
+            }
+
+            wreathsCount += storedFlowers / 15;
+
+            Console.WriteLine(wreathsCount >= 5 ?
+                $"You made it, you are going to the competition with {wreathsCount} wreaths!" :
+                $"You didn't make it, you need {5 - wreathsCount} wreaths more!");
+        }
+    }
+}
