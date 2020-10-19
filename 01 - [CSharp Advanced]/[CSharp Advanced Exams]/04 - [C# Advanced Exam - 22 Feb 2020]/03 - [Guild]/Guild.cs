5ec988691d8363d25c59b96869e7ccd32eefcfe8
george <alienguymc@gmail.com>
2020-10-20 01:02:13 +0300

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/04 - [C# Advanced Exam - 22 Feb 2020]/03 - [Guild]/Guild.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    class Guild
+    {
+        private List<Player> playersList;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.playersList = new List<Player>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get => this.playersList.Count;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (this.Capacity > this.Count)
+            {
+                this.playersList.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = this.playersList.Find(x => x.Name == name);
+
+            if (player != null)
+            {
+                this.playersList.Remove(player);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = this.playersList.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = this.playersList.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string currClass)
+        {
+            var list = new List<Player>();
+
+            foreach (var item in this.playersList)
+            {
+                if (item.Class == currClass)
+                {
+                    list.Add(item);
+                }
+            }
+
+            this.playersList.RemoveAll(x => x.Class == currClass);
+
+            return list.ToArray();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {this.Name}");
+
+            foreach (var item in this.playersList)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/04 - [C# Advanced Exam - 22 Feb 2020]/03 - [Guild]/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string currClass)
+        {
+            this.Name = name;
+            this.Class = currClass;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Rank { get; set; }
+
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Player {this.Name}: {this.Class}{Environment.NewLine}" +
+                   $"Rank: {this.Rank}{Environment.NewLine}" +
+                   $"Description: {this.Description}";
+        }
+    }
+}
