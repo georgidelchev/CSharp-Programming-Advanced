31a51278e0e2d1bb2e65443f86df6349c49977bd
george <alienguymc@gmail.com>
2020-10-20 21:01:51 +0300

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/06 - [C# Advanced Exam - 28 June 2020]/03 - [Parking]/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    public class Car
+    {
+        public Car(string manufacturer, string model, int year)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Manufacturer} {this.Model} ({this.Year})";
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/06 - [C# Advanced Exam - 28 June 2020]/03 - [Parking]/Parking.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> carsList;
+
+        public Parking(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.carsList = new List<Car>();
+        }
+
+        public string Type { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get => this.carsList.Count;
+        }
+
+        public void Add(Car car)
+        {
+            if (this.Capacity > this.Count)
+            {
+                this.carsList.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            if (this.carsList.Any(x => x.Manufacturer == manufacturer) &&
+                this.carsList.Any(x => x.Model == model))
+            {
+                Car car = this.carsList
+                    .Find(x => x.Manufacturer == manufacturer && x.Model == model);
+                this.carsList.Remove(car);
+                
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car GetLatestCar()
+        {
+            Car car = this.carsList.OrderByDescending(x => x.Year).FirstOrDefault();
+            return car;
+        }
+
+        public Car GetCar(string manufacturer,string model)
+        {
+            Car car = this.carsList
+                    .Find(x => x.Manufacturer == manufacturer && x.Model == model);
+            
+            return car;
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"The cars are parked in {this.Type}:");
+
+            foreach (var item in this.carsList)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
