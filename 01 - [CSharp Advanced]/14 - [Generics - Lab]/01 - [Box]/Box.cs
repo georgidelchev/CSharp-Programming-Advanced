e631b241cf0d3d48f39178ad5783a73272aad8bb
george <alienguymc@gmail.com>
2020-10-21 19:45:21 +0300

    add

---
+++ 01 - [CSharp Advanced]/14 - [Generics - Lab]/01 - [Box]/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+
+        public Box()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get => this.stack.Count;
+        }
+
+        public void Add(T item)
+        {
+            this.stack.Push(item);
+        }
+
+        public T Remove()
+        {
+            return this.stack.Pop();
+        }
+    }
+}
