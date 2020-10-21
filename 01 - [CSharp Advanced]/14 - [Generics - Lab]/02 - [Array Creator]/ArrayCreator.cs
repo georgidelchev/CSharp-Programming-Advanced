049ac3d2e212bb394275d30c739556889c689b28
george <alienguymc@gmail.com>
2020-10-21 19:46:02 +0300

    add

---
+++ 01 - [CSharp Advanced]/14 - [Generics - Lab]/02 - [Array Creator]/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length,T item)
+        {
+            var arr = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = item;
+            }
+
+            return arr;
+        }
+    }
+}
