940a60bd14a947d232552d5d13aad0c7f82af4a4
george <alienguymc@gmail.com>
2020-10-21 19:46:40 +0300

    add

---
+++ 01 - [CSharp Advanced]/14 - [Generics - Lab]/03 - [Generic Scale]/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var resultInt = new EqualityScale<int>(4, 4);
+            var resultString = new EqualityScale<string>("Test", "Test");
+            var resultDouble = new EqualityScale<double>(2.2, 3.3);
+
+            Console.WriteLine(resultInt.AreEqual());
+            Console.WriteLine(resultString.AreEqual());
+            Console.WriteLine(resultDouble.AreEqual());
+        }
+    }
+}
