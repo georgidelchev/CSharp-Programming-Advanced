6b5f1f6e0a723387be1d8534a31e0c49a746fd16
george <alienguymc@gmail.com>
2020-10-24 15:06:20 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/03 - [Generic Swap Method String]/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfStrings
+{
+    public class Box<T>
+    {
+        public List<T> List { get; set; }
+
+        public Box(List<T> list)
+        {
+            this.List = list;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = this.List[firstIndex];
+            this.List[firstIndex] = this.List[secondIndex];
+            this.List[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in this.List)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
