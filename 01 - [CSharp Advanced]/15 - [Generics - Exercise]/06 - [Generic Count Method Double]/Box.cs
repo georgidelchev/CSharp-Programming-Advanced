e8cc3231d0a3981e44a830a3a88138654a00f1d9
george <alienguymc@gmail.com>
2020-10-24 15:07:15 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/06 - [Generic Count Method Double]/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfStrings
+{
+    public class Box<T> where T : IComparable
+    {
+        public List<T> List { get; set; }
+
+        public Box(List<T> list)
+        {
+            this.List = list;
+        }
+
+        public int GetCount(T item)
+        {
+            int count = 0;
+
+            foreach (var currItem in this.List)
+            {
+                if (currItem.CompareTo(item) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/06 - [Generic Count Method Double]/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var list = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<double>(list);
+
+            Console.WriteLine(box.GetCount(double.Parse(Console.ReadLine())));
+        }
+    }
+}
