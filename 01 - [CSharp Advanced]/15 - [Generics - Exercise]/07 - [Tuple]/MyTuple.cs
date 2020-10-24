2709c76f9624d0404647da7623f1ff4cd2e2d9ac
george <alienguymc@gmail.com>
2020-10-24 15:07:38 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/07 - [Tuple]/MyTuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<T1,T2>
+    {
+        public MyTuple(T1 item1, T2 item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T1 Item1 { get; set; }
+
+        public T2 Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/07 - [Tuple]/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Reading();
+            var fullName = $"{input[0]} {input[1]}";
+            var address = input[2];
+
+            var myTuple1 = new MyTuple<string, string>(fullName, address);
+            Console.WriteLine(myTuple1);
+
+            input = Reading();
+            var name = input[0];
+            var litersOfBeer = int.Parse(input[1]);
+            var myTuple2 = new MyTuple<string, int>(name, litersOfBeer);
+            Console.WriteLine(myTuple2);
+
+            input = Reading();
+            var num = int.Parse(input[0]);
+            var num2 = double.Parse(input[1]);
+            var myTuple3 = new MyTuple<int, double>(num, num2);
+            Console.WriteLine(myTuple3);
+        }
+
+        private static List<string> Reading()
+            => Console
+                   .ReadLine()
+                   .Split()
+                   .ToList();
+    }
+}
