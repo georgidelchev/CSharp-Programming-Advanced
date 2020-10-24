358d80fe336d9e24f69558fa5f309d9adff02ce2
george <alienguymc@gmail.com>
2020-10-24 15:06:58 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/05 - [Generic Count Method String]/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+
+            Console.WriteLine(box.GetCount(Console.ReadLine()));
+        }
+    }
+}
