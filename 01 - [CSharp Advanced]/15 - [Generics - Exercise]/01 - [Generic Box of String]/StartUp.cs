635862c125d37c9a9b6ed5843f2eb89cf61fd4e5
george <alienguymc@gmail.com>
2020-10-24 15:04:57 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/01 - [Generic Box of String]/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
