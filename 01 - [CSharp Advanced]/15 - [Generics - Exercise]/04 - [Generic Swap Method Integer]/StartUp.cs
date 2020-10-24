a32e5e08e7882adac5b38684f5c3ee810e4331fe
george <alienguymc@gmail.com>
2020-10-24 15:06:39 +0300

    add

---
+++ 01 - [CSharp Advanced]/15 - [Generics - Exercise]/04 - [Generic Swap Method Integer]/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<int>(list);
+
+            var indexes = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var firstIndex = indexes[0];
+            var secondIndex = indexes[1];
+
+            box.Swap(firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+    }
+}
