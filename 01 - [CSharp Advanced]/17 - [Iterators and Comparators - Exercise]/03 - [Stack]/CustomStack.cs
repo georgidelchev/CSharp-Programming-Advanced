1cd2cd2b5f5fb44570fb7c205cf92e65b0bd14ce
george <alienguymc@gmail.com>
2020-10-26 14:52:08 +0200

    add

---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/03 - [Stack]/CustomStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> list;
+
+        public CustomStack()
+        {
+            this.list = new List<T>();
+        }
+
+        public int Count
+        {
+            get => this.list.Count;
+        }
+
+        public void Push(T item)
+        {
+            this.list.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = this.list[^1];
+            this.list.RemoveAt(this.list.Count - 1);
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.list.Count - 1; i >= 0; i--)
+            {
+                yield return this.list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/03 - [Stack]/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var myStack = new CustomStack<int>();
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var splittedInput = input
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                var command = splittedInput[0];
+
+                switch (command)
+                {
+                    case "Push":
+                        var elements = splittedInput
+                            .Skip(1)
+                            .ToList();
+
+                        foreach (var item in elements)
+                        {
+                            myStack.Push(int.Parse(item));
+                        }
+                        break;
+                    case "Pop":
+                        if (myStack.Count == 0)
+                        {
+                            Console.WriteLine("No elements");
+                        }
+                        else
+                        {
+                            myStack.Pop();
+                        }
+                        break;
+                }
+            }
+
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+            
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
