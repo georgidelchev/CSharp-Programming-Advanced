3759b07fb189955c3f3fcbd974afb995b3a29c01
george <alienguymc@gmail.com>
2020-10-26 15:42:00 +0200

    add

---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/05 - [Comparing Objects]/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            var comparison = this.Name.CompareTo(other.Name);
+
+            if (comparison == 0)
+            {
+                comparison = this.Age.CompareTo(other.Age);
+
+                if (comparison == 0)
+                {
+                    comparison = this.Town.CompareTo(other.Town);
+                }
+            }
+
+            return comparison;
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/05 - [Comparing Objects]/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var listOfPeople = new List<Person>();
+
+            var input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var splitted = input
+                    .Split()
+                    .ToList();
+
+                var name = splitted[0];
+                var age = int.Parse(splitted[1]);
+                var town = splitted[2];
+
+                listOfPeople.Add(new Person(name, age, town));
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            int countOfMatches = 0;
+            int numberOfNotEqualPeople = 0;
+            int totalNumberOfPeople = listOfPeople.Count;
+
+            foreach (var item in listOfPeople)
+            {
+                if (item.CompareTo(listOfPeople[n - 1]) == 0)
+                {
+                    countOfMatches++;
+                }
+                else
+                {
+                    numberOfNotEqualPeople++;
+                }
+            }
+
+            Console.WriteLine(countOfMatches == 1 ? "No matches" :
+                $"{countOfMatches} {numberOfNotEqualPeople} {totalNumberOfPeople}");
+        }
+    }
+}
