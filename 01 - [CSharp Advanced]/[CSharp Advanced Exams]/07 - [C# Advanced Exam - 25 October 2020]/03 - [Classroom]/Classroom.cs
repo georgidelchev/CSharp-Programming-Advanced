8f12cecea9fadf80d72d6eec11973ccb1aae4161
george <alienguymc@gmail.com>
2020-10-26 08:54:36 +0200

    add

---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/07 - [C# Advanced Exam - 25 October 2020]/03 - [Classroom]/Classroom.cs
@@ -0,0 +1,88 @@
+﻿using Classroom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> studentsList;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.studentsList = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get => this.studentsList.Count;
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            var result = "No seats in the classroom";
+
+            if (this.Capacity > this.Count)
+            {
+                result = $"Added student {student.FirstName} {student.LastName}";
+                this.studentsList.Add(student);
+            }
+
+            return result;
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            var result = "Student not found";
+
+            Student student = this.studentsList
+                .FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (student != null)
+            {
+                this.studentsList.Remove(student);
+                result = $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+
+            return result;
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+
+            var sb = new StringBuilder();
+            var list = this.studentsList.Where(x => x.Subject == subject).ToList();
+
+            if (!list.Any())
+            {
+                return "No students enrolled for the subject";
+            }
+
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            return
+                this.studentsList
+                .FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/[CSharp Advanced Exams]/07 - [C# Advanced Exam - 25 October 2020]/03 - [Classroom]/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classroom
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, string subject)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Subject = subject;
+        }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string Subject { get; }
+
+
+        public override string ToString()
+        {
+            return $"Student: " +
+                $"First Name = {this.FirstName}, " +
+                $"Last Name = {this.LastName}," +
+                $" Subject = {this.Subject}";
+        }
+    }
+}
