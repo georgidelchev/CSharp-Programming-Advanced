fb2d216782f70f55cf534f6421265931ebe4a90d
george <alienguymc@gmail.com>
2020-10-26 14:52:30 +0200

    add

---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/04 - [Froggy]/Lake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(List<int>elements)
+        {
+            this.stones = new List<int>(elements);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            int currentIndex = this.stones.Count-1;
+
+            if (currentIndex % 2 == 0)
+            {
+                currentIndex--;
+            }
+
+            for (int i = currentIndex; i >= 0; i -= 2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/04 - [Froggy]/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            var lake = new Lake(input);
+
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
