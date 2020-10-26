a6927021ffd7211d6862416361703663512fb8b6
george <alienguymc@gmail.com>
2020-10-26 14:51:16 +0200

    add

---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/01 - [ListyIterator]/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+
+        public ListyIterator(List<T> currList)
+        {
+            this.list = currList;
+        }
+
+        public int CurrentIndex { get; set; }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.CurrentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.list.Count > this.CurrentIndex + 1;
+        }
+
+        public void Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.list[this.CurrentIndex]);
+        }
+    }
+}
---
+++ 01 - [CSharp Advanced]/17 - [Iterators and Comparators - Exercise]/01 - [ListyIterator]/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;
+
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    var splittedInput = input
+                    .Split()
+                    .ToList();
+
+                    var command = splittedInput[0];
+
+                    switch (command)
+                    {
+                        case "Create":
+                            var elements = splittedInput
+                                .Skip(1)
+                                .ToList();
+
+                            listyIterator = new ListyIterator<string>(elements);
+                            break;
+                        case "Move":
+                            Console.WriteLine(listyIterator.Move());
+                            break;
+                        case "Print":
+                            listyIterator.Print();
+                            break;
+                        case "HasNext":
+                            Console.WriteLine(listyIterator.HasNext());
+                            break;
+                    }
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }
+            }
+        }
+    }
+}
