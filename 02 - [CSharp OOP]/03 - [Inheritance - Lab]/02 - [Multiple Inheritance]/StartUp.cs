290b270af3ca16f5ff373852c2a41765a6a659ab
george <alienguymc@gmail.com>
2020-10-31 19:08:02 +0200

    add

---
+++ 02 - [CSharp OOP]/03 - [Inheritance - Lab]/02 - [Multiple Inheritance]/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var puppy = new Puppy();
+
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
