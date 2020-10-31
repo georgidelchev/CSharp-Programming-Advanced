a9cfcec13d74059c4f1d2aa202b5e41ba0f2fe5a
george <alienguymc@gmail.com>
2020-10-31 19:12:15 +0200

    add

---
+++ 02 - [CSharp OOP]/03 - [Inheritance - Lab]/03 - [Hierarchical Inheritance]/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dog = new Dog();
+
+            dog.Eat();
+            dog.Bark();
+
+            var cat = new Cat();
+
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
