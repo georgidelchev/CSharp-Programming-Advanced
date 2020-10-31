b62ece6c3919b75d99f5d3377dcc07c368a094d2
george <alienguymc@gmail.com>
2020-10-31 19:20:07 +0200

    add

---
+++ 02 - [CSharp OOP]/03 - [Inheritance - Lab]/04 - [Random List]/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var random = new Random();
+            var index = random.Next(0, this.Count);
+            var element = this[index];
+
+            this.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/03 - [Inheritance - Lab]/04 - [Random List]/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var myList = new RandomList();
+
+            Console.WriteLine(myList.RandomString());
+        }
+    }
+}
