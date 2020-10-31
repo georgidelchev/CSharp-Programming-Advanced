1429b7b406a5b1a5591f5c482de725df7f66b413
george <alienguymc@gmail.com>
2020-10-31 19:04:07 +0200

    add

---
+++ 02 - [CSharp OOP]/03 - [Inheritance - Lab]/01 - [Single Inheritance]/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dog = new Dog();
+
+            dog.Bark();
+            dog.Bark();
+        }
+    }
+}
