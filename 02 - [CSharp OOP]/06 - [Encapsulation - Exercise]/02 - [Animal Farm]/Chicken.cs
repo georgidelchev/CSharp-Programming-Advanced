40a729d240f83084725fb4c313dab121f85e668b
george <alienguymc@gmail.com>
2020-11-02 21:48:41 +0200

    add

---
+++ 02 - [CSharp OOP]/06 - [Encapsulation - Exercise]/02 - [Animal Farm]/Chicken.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace AnimalFarm
+{
+    public class Chicken
+    {
+        private const int MIN_AGE = 0;
+        private const int MAX_AGE = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) ||
+                    string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (value < MIN_AGE || value > MAX_AGE)
+                {
+                    throw new ArgumentException($"Age should be between {MIN_AGE} and {MAX_AGE}.");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+            get
+            {
+                return this.CalculateProductPerDay();
+            }
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Chicken {this.Name} (age {this.Age}) can produce {this.ProductPerDay} eggs per day.";
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/06 - [Encapsulation - Exercise]/02 - [Animal Farm]/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalFarm
+{
+    public class Engine
+    {
+        public void Proceed()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+
+                Console.WriteLine(chicken);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
