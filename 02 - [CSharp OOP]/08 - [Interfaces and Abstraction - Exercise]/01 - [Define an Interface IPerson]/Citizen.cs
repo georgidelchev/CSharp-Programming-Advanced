b95ecb3ce8971d1efeef644b449c80032a31fc89
george <alienguymc@gmail.com>
2020-11-05 20:28:27 +0200

    add

---
+++ 02 - [CSharp OOP]/08 - [Interfaces and Abstraction - Exercise]/01 - [Define an Interface IPerson]/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public override string ToString()
+        {
+            return $"{this.Name}{Environment.NewLine}{this.Age}";
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/08 - [Interfaces and Abstraction - Exercise]/01 - [Define an Interface IPerson]/Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Engine
+    {
+        public void Proceed()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            IPerson person = new Citizen(name, age);
+
+            Console.WriteLine(person);
+        }
+    }
+}
