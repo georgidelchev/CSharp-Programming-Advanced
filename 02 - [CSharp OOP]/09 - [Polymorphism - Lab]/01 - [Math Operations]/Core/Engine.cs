45caebe2351a18218b9cf119a39c02d8a0ed4573
george <alienguymc@gmail.com>
2020-11-07 02:27:01 +0200

    add

---
+++ 02 - [CSharp OOP]/09 - [Polymorphism - Lab]/01 - [Math Operations]/Core/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations.Core
+{
+    public class Engine
+    {
+        public void Proceed()
+        {
+            MathOperations mo = new MathOperations();
+
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/09 - [Polymorphism - Lab]/01 - [Math Operations]/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Operations.Core;
+using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Proceed();
+        }
+    }
+}
