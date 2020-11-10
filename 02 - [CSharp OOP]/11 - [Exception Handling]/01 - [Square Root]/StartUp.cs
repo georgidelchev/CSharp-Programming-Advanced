c12c5c0af9aa9d921aebdec228b8db5a972f5a1c
george <alienguymc@gmail.com>
2020-11-10 17:12:57 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/01 - [Square Root]/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        private const string INVALID_MESSAGE = "Invalid number";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var number = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{Math.Sqrt(number):f2}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
