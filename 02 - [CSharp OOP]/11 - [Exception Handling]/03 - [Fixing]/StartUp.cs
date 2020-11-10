67fd0e67ee574822024eef9182fd2713a1a16a0a
george <alienguymc@gmail.com>
2020-11-10 17:13:30 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/03 - [Fixing]/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fixing
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var weekdays = new string[5];
+
+            weekdays[0] = "Sunday";
+            weekdays[1] = "Monday";
+            weekdays[2] = "Tuesday";
+            weekdays[3] = "Wednesday";
+            weekdays[4] = "Thursday";
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ior)
+            {
+                Console.WriteLine(ior.Message);
+            }
+        }
+    }
+}
