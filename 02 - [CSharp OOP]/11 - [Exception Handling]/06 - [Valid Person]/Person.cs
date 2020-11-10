0c372f53c99f3a618a2bafa6277a980278cbfa35
george <alienguymc@gmail.com>
2020-11-10 17:14:23 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/06 - [Valid Person]/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidPerson
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException($"First name cannot be empty.");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException($"Last name cannot be empty.");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException($"Age must be in the range 0 - 120.");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.FirstName} {this.LastName}{Environment.NewLine}{this.Age} yrs old person.";
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/06 - [Valid Person]/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ValidPerson
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var person = new Person("Stavri", "Stavrev", 10);
+
+                Console.WriteLine(person);
+            }
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            catch (ArgumentOutOfRangeException aoe)
+            {
+                Console.WriteLine(aoe.Message);
+            }
+        }
+    }
+}
