52c19331fc0bceb943e50c4fed338b6ddbe1cb75
george <alienguymc@gmail.com>
2020-11-10 17:13:15 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/02 - [Enter Numbers]/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    public class StartUp
+    {
+        private const string INVALID_MESSAGE = "Invalid number";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var start = int.Parse(Console.ReadLine());
+                var end = int.Parse(Console.ReadLine());
+
+                ReadNumber(start, end);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+        }
+
+        private static void ReadNumber(int start, int end)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                try
+                {
+                    var num = int.Parse(Console.ReadLine());
+
+                    while (!(start <= num && num <= end))
+                    {
+                        Console.WriteLine($"Enter valid num {start} - {end}");
+
+                        num = int.Parse(Console.ReadLine());
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(INVALID_MESSAGE);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(INVALID_MESSAGE);
+                }
+            }
+        }
+    }
+}
