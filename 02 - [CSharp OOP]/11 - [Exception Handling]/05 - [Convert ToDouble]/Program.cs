87b082168e76e1fdf5d93e678eaf0efbd8a838f9
george <alienguymc@gmail.com>
2020-11-10 17:14:04 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/05 - [Convert ToDouble]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    public class Program
+    {
+        private const string INVALID_MESSAGE = "Invalid number";
+
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            try
+            {
+                var number = Convert.ToDouble(input);
+                Console.WriteLine(number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+                throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+                throw;
+            }
+        }
+    }
+}
