3a2ac553f2ff0bccf85ec74c4137ef3a373a2bbe
george <alienguymc@gmail.com>
2020-11-10 17:13:47 +0200

    add

---
+++ 02 - [CSharp OOP]/11 - [Exception Handling]/04 - [Fixing Vol2]/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+
+                Console.WriteLine($"{num1}x{num2}={result}");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+            }
+        }
+    }
+}
