f506dc5374111870667482d8d1aea88af1dacc8e
george <alienguymc@gmail.com>
2020-11-13 17:44:06 +0200

    add

---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/01 - [Stealer]/Spy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            var classType = Type.GetType(className);
+
+            FieldInfo[] classFields = classType
+                .GetFields(BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.NonPublic |
+                BindingFlags.Public);
+
+            var classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {className}");
+
+            foreach (var item in classFields
+                .Where(f => fieldsNames.Contains(f.Name)))
+            {
+                sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/01 - [Stealer]/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
