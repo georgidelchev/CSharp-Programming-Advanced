fff0f409319ffb1f900e1790c5f4ef73fe5eac3e
george <alienguymc@gmail.com>
2020-11-13 18:50:58 +0200

    add

---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/05 - [Create Attribute]/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
