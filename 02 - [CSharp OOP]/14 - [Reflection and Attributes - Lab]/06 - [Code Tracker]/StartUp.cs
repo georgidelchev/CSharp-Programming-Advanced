ed66edf3ab8bfb80faf9ac51e4ab9ff6741d9032
george <alienguymc@gmail.com>
2020-11-13 19:01:14 +0200

    add

---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/06 - [Code Tracker]/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/06 - [Code Tracker]/Tracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+
+            var methods = type
+                .GetMethods(BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.Static);
+
+            foreach (var item in methods)
+            {
+                if (item.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = item.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute item1 in attributes)
+                    {
+                        Console.WriteLine($"{item.Name} is written by {item1.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
