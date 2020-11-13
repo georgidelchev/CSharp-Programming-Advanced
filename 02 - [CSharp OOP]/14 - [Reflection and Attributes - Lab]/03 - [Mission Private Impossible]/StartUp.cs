71d31a823a9b05fc198101b3756a4a6faac58c38
george <alienguymc@gmail.com>
2020-11-13 18:37:02 +0200

    адд

---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/03 - [Mission Private Impossible]/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
