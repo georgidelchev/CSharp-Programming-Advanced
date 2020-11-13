336daf9ee9c1be62ef2e71e8f8cb411698a5d6f1
george <alienguymc@gmail.com>
2020-11-13 18:45:46 +0200

    add

---
+++ 02 - [CSharp OOP]/14 - [Reflection and Attributes - Lab]/04 - [Collector]/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
